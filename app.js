c7a313a1e7c34e9705ba7ceb6b51bd150e4ac334
Mohamed Tarek Mahmoud Ahmed Eldahshan <2400046@student.eelu.edu.eg>
2026-06-17 03:19:17 +0300

    First version

---
+++ app.js
@@ -0,0 +1,77 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
+import {
+getFirestore,
+doc,
+setDoc,
+onSnapshot
+}
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD9XAlpeDn4UFp-0taUmCvUm86XkPTNboM",
+  authDomain: "our-status-a9535.firebaseapp.com",
+  projectId: "our-status-a9535",
+  storageBucket: "our-status-a9535.firebasestorage.app",
+  messagingSenderId: "402776687762",
+  appId: "1:402776687762:web:556c3e1449e3fc9e58924b"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.updateStatus = async function(person,status){
+
+await setDoc(doc(db,"status",person),{
+status:status,
+time:new Date().toLocaleString()
+});
+
+};
+
+window.sendMessage = async function(person){
+
+let message=document.getElementById(person+"Msg").value;
+
+await setDoc(doc(db,"messages",person),{
+message:message,
+time:new Date().toLocaleString()
+});
+
+};
+
+onSnapshot(doc(db,"status","mohamed"),(docSnap)=>{
+
+if(docSnap.exists()){
+document.getElementById("mohamedStatus").innerHTML=
+docSnap.data().status+"<br>"+docSnap.data().time;
+}
+
+});
+
+onSnapshot(doc(db,"status","her"),(docSnap)=>{
+
+if(docSnap.exists()){
+document.getElementById("herStatus").innerHTML=
+docSnap.data().status+"<br>"+docSnap.data().time;
+}
+
+});
+
+onSnapshot(doc(db,"messages","mohamed"),(docSnap)=>{
+
+if(docSnap.exists()){
+document.getElementById("mohamedMessage").innerHTML=
+docSnap.data().message;
+}
+
+});
+
+onSnapshot(doc(db,"messages","her"),(docSnap)=>{
+
+if(docSnap.exists()){
+document.getElementById("herMessage").innerHTML=
+docSnap.data().message;
+}
+
+});
